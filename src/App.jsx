d80ae57f78594736f727e4effd60991880e65fb3
Oleh Purys <wenger1901@gmail.com>
2024-09-14 06:25:00 +0300

    push hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import userData from './assets/user.json';
-import Profile from './components/Profile/Profile';
-import friends from './assets/friends.json';
-import FriendList from './components/FriendList/FriendList';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './assets/transactions.json';
+import { useEffect, useState } from 'react';
+import initialContacts from './assets/contacts.json';
+import ContactForm from './components/ContactForm/ContactForm';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactList from './components/ContactList/ContactList';
 
 const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? initialContacts
+  );
+
+  const [filter, setFilter] = useState('');
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const addContacts = newContact => {
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const deleteContacts = contactId => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(contact => contact.id !== contactId);
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContacts} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={visibleContacts} onDelete={deleteContacts} />
+    </div>
   );
 };
 
